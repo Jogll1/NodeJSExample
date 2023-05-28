097a65fa30b704b67f2c62214568f0e6d29b6cda
JoelLuckett <joelluckett@icloud.com>
2023-05-28 14:52:25 +0100

    Added html and css and updated index.js to work

---
+++ index.js
@@ -0,0 +1,19 @@
+//express
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+
+//using the public folder
+app.use(express.static(__dirname + '/public'));
+
+//send html page
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/html/index.html');
+});
+
+//open port 3000
+server.listen(3000, () => {
+    console.log('App available on http://localhost:3000');
+});
